34b8411722fe45190dcf68d9d50cd5f6ac88db18
Prashaint Mishra <prashaint_mishra@hotmail.com>
2020-08-22 21:59:24 +0530

    Added few new pages

--- django_prashaint_portfolio/urls.py
+++ django_prashaint_portfolio/urls.py
@@ -20,8 +20,9 @@ from django.conf import settings
 from portfolio import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
     path('blogs/', include('blogs.urls')),
 ]
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,3 +4,7 @@ from .models import Project
 def home(request):
 	projects = Project.objects.all()
 	return render(request, 'portfolio/home.html', {'projects': projects})
+
+def about(request):
+	projects = Project.objects.all()
+	return render(request, 'portfolio/about.html', {'projects': projects})
