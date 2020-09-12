fcc00bfa6aea45fffa9c3f3ea907a27ee417ed2b
Prashaint Mishra <prashaint_mishra@hotmail.com>
2020-09-12 13:37:29 +0530

    Navbar style changes. Footer style changes

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import BlogPost
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status','created_on')
+    list_display = ('title', 'status','created_on', )
     list_filter = ("status",)
     search_fields = ['title', 'content']
     #prepopulated_fields = {(title:'title')}
